a2d4326760cc5e9fdcd762a80b5d672db6c71ee8
jonathanParamo <jhonathan-and@outlook.com>
2024-01-26 23:59:38 -0500

    Preparing to start the project

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const response = await fetch("http://localhost:8080/todos/1");
+      const data = await response.json();
+      console.log("Datos recibidos:", data);
+      setTodos(data);
+    } catch (error) {
+      console.error("Error al obtener los datos:", error);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+    <Text>Hola mundo</Text>
+    <FlatList
+      data={todos}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <View>
+          <Text>{item.title}</Text>
+        </View>
+      )}
+      ListHeaderComponent={() => <Text style={styles.title}>Today</Text>}
+      contentContainerStyle={styles.contentContainerStyle}
+    />
+    <StatusBar style="auto" />
+  </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
   },
+  contentContainerStyle: {
+    padding: 15,
+  },
+  title: {
+    fontWeight: "800",
+    fontSize: 28,
+    marginBottom: 15,
+  }
 });
